dcd422bfb3cea9ccd6082d3f659291d412ff0487
jamesnguyendev <jamesnguyen.devs@gmail.com>
2025-03-21 21:00:18 +0700

    fixed text header cart

--- components/headers/HeaderCart.tsx
+++ components/headers/HeaderCart.tsx
@@ -33,8 +33,8 @@ const HeaderCart = ({ cart }: { cart: any }) => {
       </div>
 
       {/* Giỏ hàng hiển thị khi hover */}
-      {cart?.lines?.length === 0 ? (
-        <div className="pointer-events-none text-black absolute right-0 top-12 min-w-max rounded-lg border-b-2 border-amber-400 bg-white p-4 opacity-0 shadow-lg transition-opacity group-hover:pointer-events-auto group-hover:opacity-100 max-lg:hidden">
+      {cart?.lines?.length === 0 || cart?.lines?.length === null ? (
+        <div className="pointer-events-none absolute right-0 top-12 min-w-max rounded-lg border-b-2 border-amber-400 bg-white p-4 text-black opacity-0 shadow-lg transition-opacity group-hover:pointer-events-auto group-hover:opacity-100 max-lg:hidden">
           <div className="absolute -top-2 right-4 h-0 w-0 border-b-8 border-l-8 border-r-8 border-b-white border-l-transparent border-r-transparent"></div>
           <h1>Chưa có sản phẩm</h1>
         </div>
